2b0ae708020cdc4b861e167c7f6e21e12fab12a0
Mayur Agarwal <mayur.m.agarwal@gmail.com>
2025-07-06 23:56:04 +0530

    env bug fixes

--- next.config.js
+++ next.config.js
@@ -3,18 +3,15 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com'
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: 'cloud.appwrite.io'
-      }
-    ]
+        protocol: "https",
+        hostname: "cloud.appwrite.io",
+      },
+    ],
   },
-  env: {
-    API_BASE_URL: process.env.API_BASE_URL,
-  }
-}
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
