dfbfdd1111d3a64617df2c6408018e83e414c98a
Mayur Agarwal <mayur.m.agarwal@gmail.com>
2026-03-05 19:13:03 +0530

    hide sign in with google option

--- app/sign-in/page.js
+++ app/sign-in/page.js
@@ -188,7 +188,7 @@ function SignInContent() {
               )}
             </Button>
 
-            <div className="relative">
+            {/* <div className="relative">
               <div className="absolute inset-0 flex items-center">
                 <span className="w-full border-t border-purple-200" />
               </div>
@@ -197,9 +197,9 @@ function SignInContent() {
                   Or continue with
                 </span>
               </div>
-            </div>
+            </div> */}
 
-            <Button
+            {/* <Button
               type="button"
               variant="outline"
               className="w-full border-purple-200 hover:bg-purple-50 gap-2"
@@ -225,7 +225,7 @@ function SignInContent() {
                 />
               </svg>
               Sign in with Google
-            </Button>
+            </Button> */}
           </form>
         </div>
 
